3c59061584036cb1751ed6c1d8c4c7771177b76e
Taizul52 <islam.taijul133052@gmail.com>
2019-10-30 13:50:09 +0600

    add question model and then migrate to sqlite database

---
+++ app/Question.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Question extends Model
+{
+    public function user(){
+        return $this->belongsTo(\App\User::class);
+    }
+
+    public function setTitleAttribute($value){
+        $this->attributes['title']=$value;
+        $this->attributes['slug']=Str::slug($value);
+    }
+}
